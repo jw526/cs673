5c63273ebf099245182e3dee73c99e8f968dadd0
Mike <mikecit22@gmail.com>
2018-09-23 01:33:30 -0400

    Revert "added templates"
    This reverts commit 92537cea3537f1de092e031277361d0ae223c725.

--- pages/partials/footer.php
+++ pages/partials/footer.php
@@ -1,5 +1,5 @@
-<script type="text/javascript" src="/~mc332/cs673/libs/jquery.js" ></script>
-<script type="text/javascript" src="/~mc332/cs673/libs/bootstrap/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/~mc332/cs673/controllers/app.js"></script>
-<script type="text/javascript" src="/~mc332/cs673/controllers/user.js"></script>
-<script type="text/javascript" src="/~mc332/cs673/controllers/portfolios.js"></script>
+<script type="text/javascript" src="/libs/jquery.js" ></script>
+<script type="text/javascript" src="/libs/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/controllers/app.js"></script>
+<script type="text/javascript" src="/controllers/user.js"></script>
+<script type="text/javascript" src="/controllers/portfolios.js"></script>
--- pages/partials/header.php
+++ pages/partials/header.php
@@ -3,8 +3,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>My Stock App</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="/~mc332/cs673/index.css" />
-  <link rel="stylesheet" type="text/css" media="screen" href="/~mc332/cs673/libs/bootstrap/css/bootstrap-grid.min.css" />
-  <link rel="stylesheet" type="text/css" media="screen" href="/~mc332/cs673/libs/bootstrap/css/bootstrap-reboot.min.css" />
-  <link rel="stylesheet" type="text/css" media="screen" href="/~mc332/cs673/libs/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="/index.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="/libs/bootstrap/css/bootstrap-grid.min.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="/libs/bootstrap/css/bootstrap-reboot.min.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="/libs/bootstrap/css/bootstrap.min.css" />
 </head>

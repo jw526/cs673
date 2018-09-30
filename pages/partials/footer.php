c747212094296ed4a8b51e8a42650c1a230ccf55
Mike <mikecit22@gmail.com>
2018-09-30 11:37:57 -0400

    added support for india stock search

--- pages/partials/footer.php
+++ pages/partials/footer.php
@@ -10,43 +10,12 @@
     include(dirname(__FILE__). '/../../controllers/app.js');
     include(dirname(__FILE__). '/../../controllers/user.js');
     include(dirname(__FILE__). '/../../controllers/portfolios.js');
+    include(dirname(__FILE__). '/./supportedStocks.js');
   ?>
 
     $( function() {
-    var availableTags = [
-      'MMM: 3M',
-      'AXP: American Express',
-      'AAPL: Apple',
-      'BA: Boeing',
-      'CAT: Caterpillar',
-      'CVX: Chevron',
-      'CSCO: Cisco',
-      'KO: Coca-Cola',
-      'DIS: Disney',
-      'DWDP: DowDuPont Inc',
-      'XOM: Exxon Mobil',
-      'GS: Goldman Sachs',
-      'HD: Home Depot',
-      'IBM: IBM',
-      'INTC: Intel',
-      'JNJ: Johnson &amp; Johnson',
-      'JPM: JPMorgan Chase',
-      'MCD: McDonald\'s',
-      'MRK: Merck',
-      'MSFT: Microsoft',
-      'NKE: Nike',
-      'PFE: Pfizer',
-      'PG: Procter &amp; Gamble',
-      'TRV: Travelers Companies Inc',
-      'UTX: United Technologies',
-      'UNH: UnitedHealth',
-      'VZ: Verizon',
-      'V: Visa',
-      'WMT: Wal-Mart',
-      'WBA: Walgreen'
-    ];
-    $( "#search-input-feild" ).autocomplete({
-      source: availableTags
+      $( "#search-input-feild" ).autocomplete({
+        source: dow30.concat(indiaStocks)
+      });
     });
-  } );
 </script>

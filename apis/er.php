be05f2fabee2df0912a0f7d116012d9e1e80e068
jw526 <jw526@njit.edu>
2018-12-17 17:32:37 -0500

    updated expected return api

---
+++ apis/er.php
@@ -0,0 +1,20 @@
+<?php
+
+$function = $_POST['function'];
+$us_stocks = $_POST['us_stocks'];
+$india_stocks = $_POST['india_stocks'];
+
+function exec_r (){
+    global $us_stocks, $india_stocks;
+
+    exec('Rscript ER.r $us_stocks $india_stocks', $output);
+
+    echo "Stock Expected Return(Daily):", PHP_EOL;
+
+    for ($i=0; $i < count($output); $i++) {
+        // $line = explode ( " " , $output[$i] );
+        echo $output[$i], PHP_EOL;
+    }
+}
+
+?>
